caf644d4b23e46a6a141e9e3020cb5b58f6dde62
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-08-11 16:50:46 +0530

    feat(firebase): adds firebase configs to the project

---
+++ frontend/app/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getAnalytics, isSupported } from "firebase/analytics"
+
+type IAuthState = {
+  user: any,
+  pending: boolean,
+  isSignedIn: boolean,
+}
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementID: process.env.FIREBASE_MEASUREMENT_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, db, auth, analytics, type IAuthState }
